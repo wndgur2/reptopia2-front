084e9381a77a59f237475f2eca7e4b32740418b5
leejunghyeok <wndgur76@naver.com>
2022-08-08 12:34:57 +0900

    ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,13 +14,13 @@ export interface Tile {
 })
 export class HomeComponent{
   tiles: Tile[] = [
-    {text: 'One', cols: 120, rows: 80, color: 'lightblue'},
+    {text: '도돌이, 양이', cols: 90, rows: 60, color: 'lightblue'},
     {text: 'Two', cols: 45, rows: 60, color: 'lightgreen'},
-    {text: 'Three', cols: 100, rows: 100, color: 'lightpink'},
+    {text: 'Three', cols: 90, rows: 60, color: 'lightpink'},
     {text: 'Four', cols: 60, rows: 45, color: '#DDBDF1'},
     {text: 'One', cols: 120, rows: 80, color: 'lightblue'},
     {text: 'Two', cols: 45, rows: 60, color: 'lightgreen'},
-    {text: 'Three', cols: 100, rows: 100, color: 'lightpink'},
+    {text: 'Three', cols: 30, rows: 45, color: 'lightpink'},
     {text: 'Four', cols: 60, rows: 45, color: '#DDBDF1'},
     {text: 'One', cols: 120, rows: 80, color: 'lightblue'},
     {text: 'Two', cols: 45, rows: 60, color: 'lightgreen'},
